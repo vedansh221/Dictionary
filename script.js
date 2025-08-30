782402e0578f0d85c4e977d4ffb17d236d3947f8
vedansh221 <v3577513@gmail.com>
2025-08-30 14:40:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,51 +9,65 @@ const dictionaryArea = document.querySelector('.dictionary-app')
 // https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 
 
+
+async function dictionaryFn(word){
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(res => res.json())
+
+    return res[0]
+}
+
+
+btn.addEventListener('click' , fetchandCreateCard)
+
+
+
 async function fetchandCreateCard(){
-    const data = await dictionaryFn(input.value);
+    const data = await dictionaryFn(input.value)
 
-    if (!data || !data.meanings) {
-        dictionaryArea.innerHTML = "<div class='card'>No definition found.</div>";
-        return;
+    console.log(data)
+
+
+    let partOfSpeechArray = []
+
+    for(let i=0 ; i<data.meanings.length-1 ; i++){
+        partOfSpeechArray.push(data.meanings[i].partOfSpeech)
     }
 
-    let partOfSpeechArray = [];
-    for(let i = 0; i < data.meanings.length; i++) {
-        partOfSpeechArray.push(data.meanings[i].partOfSpeech);
-    }
 
     dictionaryArea.innerHTML = `
     <div class="card">
-        <div class="property">
-            <span>Word:</span>
-            <span>${data.word}</span>
-        </div>
-        <div class="property">
-            <span>phonetics:</span>
-            <span>${data.phonetic || ""}</span>
-        </div>
-        <div class="property">
-            <span>
-            <audio controls src="${data.phonetics && data.phonetics[0] ? data.phonetics[0].audio : ""}"></audio>
-            </span>
-        </div>
-        <div class="property">
-            <span>Definition</span>
-            <span>${data.meanings[0].definitions[0].definition}</span>
-        </div>
-        <div class="property">
-            <span>Example</span>
-            <span>${
-                data.meanings[1] && data.meanings[1].definitions[0].example
-                ? data.meanings[1].definitions[0].example
-                : "No example available"
-            }</span>
-        </div>
-        <div class="property">
-            <span>${partOfSpeechArray.join(', ')}</span>
-        </div>
-    </div>
-    `;
-}
+                   <div class="property">
+                    <span>Word:</span>
+                    <span>${data.word}</span>
+                   </div>
+
+                   <div class="property">
+                    <span>phonetics:</span>
+                    <span>${data.phonetic}</span>
+                   </div>
+
+
+                   <div class="property">
+                    <span>
+                    <audio controls src="${data.phonetics[0].audio}"></audio>
+                    </span>
+                   </div>
+
+                   <div class="property">
+                    <span>Definition</span>
+                    <span>${data.meanings[0].definitions[0].definition}</span>
+                   </div>
+
+                   <div class="property">
+                    <span>Example</span>
+                    <span>${data.meanings[1].definitions[0].example}</span>
+                   </div>
 
                    
+                   <div class="property">
+                    <span>${partOfSpeechArray.map(e => e).join(', ')}</span>
+                   </div>
+              </div>
+    `
+}
